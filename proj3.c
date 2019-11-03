e14cad3ff3ad4b937c97f1550e4ba14e6c5d8b81
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-11-03 19:04:15 +0500

    requirement 1

---
+++ proj3.c
@@ -0,0 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    char *filename = "copyfile.txt";
+
+    int fd = creat(filename, S_IRWXU);
+
+    if (fd == -1)
+    {
+        printf("Failed");
+        exit(1);
+    }
+    int save_stdout = dup(1);
+    dup2(fd, STDOUT_FILENO);
+    for (int i = 1; i < argc; i++)
+    {
+        printf("%s", argv[i]);
+    }
+    fflush(stdout);
+    dup2(save_stdout, 1); //also use (STDOUT_FILENO) for output like we use 1 for output bcz sometimes 1 is not use for output
+    printf("Output Successfull\n");
+    close(fd);
+    return 0;
+}
