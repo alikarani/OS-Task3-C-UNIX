b8564f5a7b40797066b57281046596e9e13f5748
tannokhi <khantanveer8380@gmail.com>
2019-11-24 22:42:55 +0500

    pipe code comments added

--- proj3.c
+++ proj3.c
@@ -94,24 +94,28 @@ int main(int argc, char *argv[])
 
     int pid1 = fork();
 
-    if (pid1 != 0) 
+    //Is Parent process me humne pipe1 k read end ko close kia or pipe2 k write end ko close kia
+    //write kia pipe1 se or read kia pipe2 se
+    if (pid1 != 0) //pipe2=read , pipe1=write
     {
-        close(pipe_1[0]); 
-        close(pipe_2[1]); 
+        close(pipe_1[0]); //close kia pipe1 k read end ko
+        close(pipe_2[1]); //close kia pipe2 k write end ko
         printf("Write on parent process Pipe 1:%s\n", w_msg1);
-        write(pipe_1[1], w_msg1, sizeof(w_msg1)); 
-        read(pipe_2[0], r_msg, sizeof(r_msg));    
+        write(pipe_1[1], w_msg1, sizeof(w_msg1)); //write kia pipe1 p
+        read(pipe_2[0], r_msg, sizeof(r_msg));    //read kia pipe2 se
         printf("Read from  parent process Pipe 2:%s\n", r_msg);
     }
 
+    //Is child process me humne pipe1 k write end ko close kia or pipe2 k read end ko close kia
+    //write kia pipe2 se or read kia pipe1 se
     else
     {
-        close(pipe_1[1]);                      
-        close(pipe_2[0]);                      
-        read(pipe_1[0], r_msg, sizeof(r_msg)); 
+        close(pipe_1[1]);                      //close kia pipe1 k writre end ko
+        close(pipe_2[0]);                      //close kia pipe2 k read end ko
+        read(pipe_1[0], r_msg, sizeof(r_msg)); //read kia pipe1 se
         printf("Read from child process Pipe 1:%s\n", r_msg);
         printf("Write on child process Pipe 2:%s\n", w_msg2);
-        write(pipe_2[1], w_msg2, sizeof(w_msg2)); 
+        write(pipe_2[1], w_msg2, sizeof(w_msg2)); //write kia pipe2 p
     }
 
     int save_stdout = dup(1);
