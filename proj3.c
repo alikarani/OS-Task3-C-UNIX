b89a500ef742acc0ed0f1362b8da934fb5b5820f
aliasgharkarani <aliasgharkarani125@hotmail.com>
2019-11-24 16:39:07 +0500

    sigaction added

--- proj3.c
+++ proj3.c
@@ -4,8 +4,26 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <signal.h>
+
+static void pSigHandler(int signo){
+    switch (signo) {
+            case SIGTSTP:
+            printf("TSTP");
+            fflush(stdout);
+            break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
+
+    // struct sigaction psa;
+    // psa.sa_handler = pSigHandler;
+    // sigaction(SIGTSTP, &psa, NULL);
+    // for(;;) {}
+
+
     char *filename = "copyfile.txt";
 
     int fd = creat(filename, S_IRWXU);
@@ -16,7 +34,17 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
- pid_t pid;
+    printf("It created a child process with fork!\n");
+    // pid_t cpid;
+    // if (fork() == 0)
+    //     exit(0); /* terminate child */
+
+    // else
+    //     cpid = wait(NULL); /* reaping parent */
+    // printf("Parent pid = %d\n", getpid());
+    // printf("Child pid = %d\n", cpid);
+
+    pid_t pid;
     pid = fork();
     if (pid < 0)
     {
@@ -41,8 +69,6 @@ int main(int argc, char *argv[])
         printf("child complete\n");
     }
 
-
-
     int save_stdout = dup(1);
     dup2(fd, STDOUT_FILENO);
     for (int i = 1; i < argc; i++)
@@ -55,3 +81,6 @@ int main(int argc, char *argv[])
     close(fd);
     return 0;
 }
+//METHODS NOT USED
+//open() As the functionality of open and creat was similar so Ali has used Creat.
+//
